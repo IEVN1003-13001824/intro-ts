3819ff9bc17d2bb25345349fb2766c92f3de27f2
Daniela Lopez <13001824@alumnos.utleon.edu.mx>
2024-09-06 19:07:30 -0600

    Repositorio de intro-ts

---
+++ src/temas-ts/01-tiposDat.ts
@@ -0,0 +1,17 @@
+var num1=38
+var num2=29
+const num3=10
+
+let sum = num1+num2+num3;
+let nombre: string
+let num:number
+let activo:boolean
+nombre = 'Daniela'
+num =27
+activo=true
+
+let matricula:number|string|boolean
+matricula=24423424
+matricula ='matricula'
+
+console.log("el numero es",sum)
---
+++ src/temas-ts/02-interfaz.ts
@@ -0,0 +1,12 @@
+interface Alumno{
+    nombre:string;
+    edad: number;
+    email: string;
+    calificacion:number;
+}
+
+const alumno:Alumno={
+    nombre:'Daniela',
+    edad:100,
+    email:'correo@gmail.com'
+}
